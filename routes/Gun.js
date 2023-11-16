a350d68b18a4e9a8baf4619a57b652bad50e9466
HariniBeeram <143022203+HariniBeeram@users.noreply.github.com>
2023-11-15 18:41:06 -0600

    added

--- routes/Gun.js
+++ routes/Gun.js
@@ -1,16 +1,10 @@
 var express = require('express');
 const Gun_controllers= require('../controllers/Gun');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Gun', { title: 'Search Results Gun' });
-});
-
 /* GET Gun */
-router.get('/', Gun_controllers.Gun_view_all_Page );
+router.get('/', Gun_controllers.Gun_view_one_Page );
+// GET request for one Gun.
 router.get('/Gun/:id', Gun_controllers.Gun_detail);
-
-/* GET detail Gun page */
-router.get('/detail', Gun_controlers.Gun_view_one_Page);
 module.exports = router;
+/* GET detail Gun page */
+router.get('/detail', Gun_controllers.Gun_view_one_Page);
